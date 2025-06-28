1f6ce709b72f620fe6f0058a0106496f6c94d8ee
TigerAsks <155941448+tigerasks@users.noreply.github.com>
2025-06-28 12:42:36 +0200

    Check for valid input length

--- src/dna-translator/dna-translator.ts
+++ src/dna-translator/dna-translator.ts
@@ -1,10 +1,14 @@
-import {Component, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
+import {assert} from '@angular/compiler-cli/linker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DnaTranslator {
   public dnaToAa(dna: string) : string {
+    if(dna.length % 3 !== 0){
+      throw Error('input length must be a multiple of 3')
+    }
 
     return ''
   }
