643be919e280341b16d9119bb99862bbc2ea01ad
TigerAsks <155941448+tigerasks@users.noreply.github.com>
2025-06-28 13:15:33 +0200

    Move lookup table to dedicated file

--- src/dna-translator/dna-translator.ts
+++ src/dna-translator/dna-translator.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {dnaTripletToAaSymbol} from './dna-to-aa-map';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,7 @@ export class DnaTranslator {
 
       if(i % 3 == 0){
         const triplet = next.join('')
-        const aa = dnaToAa.get(triplet)
+        const aa = dnaTripletToAaSymbol.get(triplet)
         if(aa === undefined){
           throw new Error(`Failed to translate triplet ${triplet}`)
         }
@@ -33,70 +34,3 @@ export class DnaTranslator {
     return result.join('')
   }
 }
-
-const dnaToAa = new Map([
-  ["TTT", "F"],
-  ["TTC", "F"],
-  ["TTA", "L"],
-  ["TTG", "L"],
-  ["TCT", "S"],
-  ["TCC", "S"],
-  ["TCA", "S"],
-  ["TCG", "S"],
-  ["TAT", "Y"],
-  ["TAC", "Y"],
-  ["TAA", "*"],
-  ["TAG", "*"],
-  ["TGT", "C"],
-  ["TGC", "C"],
-  ["TGA", "*"],
-  ["TGG", "W"],
-  ["CTT", "L"],
-  ["CTC", "L"],
-  ["CTA", "L"],
-  ["CTG", "L"],
-  ["CCT", "P"],
-  ["CCC", "P"],
-  ["CCA", "P"],
-  ["CCG", "P"],
-  ["CAT", "H"],
-  ["CAC", "H"],
-  ["CAA", "Q"],
-  ["CAG", "Q"],
-  ["CGT", "R"],
-  ["CGC", "R"],
-  ["CGA", "R"],
-  ["CGG", "R"],
-  ["ATT", "I"],
-  ["ATC", "I"],
-  ["ATA", "I"],
-  ["ATG", "M"],
-  ["ACT", "T"],
-  ["ACC", "T"],
-  ["ACA", "T"],
-  ["ACG", "T"],
-  ["AAT", "N"],
-  ["AAC", "N"],
-  ["AAA", "K"],
-  ["AAG", "K"],
-  ["AGT", "S"],
-  ["AGC", "S"],
-  ["AGA", "R"],
-  ["AGG", "R"],
-  ["GTT", "V"],
-  ["GTC", "V"],
-  ["GTA", "V"],
-  ["GTG", "V"],
-  ["GCT", "A"],
-  ["GCC", "A"],
-  ["GCA", "A"],
-  ["GCG", "A"],
-  ["GAT", "D"],
-  ["GAC", "D"],
-  ["GAA", "E"],
-  ["GAG", "E"],
-  ["GGT", "G"],
-  ["GGC", "G"],
-  ["GGA", "G"],
-  ["GGG", "G"],
-])
