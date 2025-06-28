1bf6415b7392043f76da9b43fe34c563b97ecc19
TigerAsks <155941448+tigerasks@users.noreply.github.com>
2025-06-28 12:48:25 +0200

    Check for valid input DNA

--- src/dna-translator/dna-translator.ts
+++ src/dna-translator/dna-translator.ts
@@ -5,9 +5,14 @@ import {assert} from '@angular/compiler-cli/linker';
   providedIn: 'root'
 })
 export class DnaTranslator {
+  private static invalidDnaPattern = new RegExp(/[^CGAT]/g)
+
   public dnaToAa(dna: string) : string {
     if(dna.length % 3 !== 0){
       throw Error('input length must be a multiple of 3')
+    }
+    if(DnaTranslator.invalidDnaPattern.test(dna)){
+      throw Error('input is not a valid DNA sequence')
     }
 
     return ''
